0a42b8011f1e719ceafa0a571220bdbd729292c6
Catarina Conceição <pc.catarina96@hotmail.com>
2018-05-07 20:39:12 +0100

    Add files via upload

--- fruitBots.py
+++ fruitBots.py
@@ -2,276 +2,240 @@ import pygame
 from bot import Bot
 from slidersFile import Slider
 from random import randint
+import numpy as np
 
 # grid size
 WINDOW_SIZE = [720, 516]
 
-width = 30
-height = 30
-margin = 5
-
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-bright_green = (0, 200, 0)
-bright_red = (200, 0, 0)
-
-player_img = pygame.image.load('img/bot.png')
-banana_img = pygame.image.load('img/banana.png')
+width = 40
+height = 40
+margin = 2
 
 # create grid
-gridSize = 10
-grid = [[0 for x in range(gridSize)] for y in range(gridSize)]
+grid_size = 10
+grid = [[0 for x in range(grid_size)] for y in range(grid_size)]
 
-# timer variables
-passed_time = 0
-timer_started = False
+# variável de controlo do ciclo principal
+end = False
 
-font = pygame.font.SysFont("comicsansms", 32)
-fontScore = pygame.font.SysFont("arial", 20)
-font_color = pygame.Color('springgreen')
+# bot
+bot = Bot()
+bot.setBoard_size(grid_size - 1)
+
+MAX_TIME = 10
+time_left = MAX_TIME
+start_time = 0
+game_start = False
+
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+# nova imagem a mostrar
+new_frame = None
+
+clock = pygame.time.Clock()
+pygame.display.set_caption("Fruit Bots")
+
+white = (255, 255, 255)
+dark_grey = (30,34,38)
+grey = (52, 56, 60)
+light_grey = (171, 178, 186)
+
+logo = pygame.image.load("img/logo_watermelon.png")
+#logo = pygame.transform.scale(logo, (banner_base_size, banner_base_size))
+banana_img = pygame.image.load('img/banana2.png')
+banana_img = pygame.transform.scale(banana_img, (30, 30))  
+img_banana_size = banana_img.get_size()[0]
+
+
+banner_base_size = 65
+grid_offset_x = 10
+grid_offset_y = 75
+
+font_title_size = 20
+font_title = pygame.font.Font("font/Roboto-Regular.ttf", font_title_size)
+
+# for random actions
+last_action_time = pygame.time.get_ticks()
 
 # Sliders settings
 num_bananas = Slider("Bananas", 2, 5, 1, 25)
 num_apples = Slider("Apples", 2, 5, 1, 175)
 num_watermelons = Slider("Watermelons", 2, 5, 1, 325)
-
 slides = list()
 slides.append(num_bananas)
 slides.append(num_apples)
 slides.append(num_watermelons)
 
-pygame.init()
-screen = pygame.display.set_mode(WINDOW_SIZE)
-done = False
-clock = pygame.time.Clock()
-pygame.display.set_caption("Fruit Bots")
+num_fruits = TOTAL_FRUITS = num_bananas.val
+
+def process_pygame_events():
+    
+    global game_start
+    global end
+    global start_time
+    global time_left
+    global last_action_time
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            end = True
+            
+        pressed = pygame.key.get_pressed()
+        
+        if pressed[pygame.K_SPACE]:
+            print("PRESS SPACE")
+            
+            game_start = not game_start
+            if not game_start: new_game()
+            else: 
+                start_time = pygame.time.get_ticks()
+                time_left = MAX_TIME
+                last_action_time = pygame.time.get_ticks()
+        
+        if game_start:
+            if pressed[pygame.K_DOWN]:    
+                print("PRESS ARROW")
+                grid[bot.row][bot.column] = 0
+                bot.move_down()
+
+            elif pressed[pygame.K_UP]:    
+                print("PRESS ARROW")
+                grid[bot.row][bot.column] = 0
+                bot.move_up()
+
+            elif pressed[pygame.K_RIGHT]:    
+                print("PRESS ARROW")
+                grid[bot.row][bot.column] = 0
+                bot.move_right()
+
+            elif pressed[pygame.K_LEFT]:    
+                print("PRESS ARROW")
+                grid[bot.row][bot.column] = 0
+                bot.move_left()
+        
+
+def draw_banner():
+    global new_frame
+    global time_left
+    global game_start
+    
+    new_frame.fill(dark_grey, (0, 0, banner_base_size, banner_base_size))
+    new_frame.fill(grey, (banner_base_size, 0, WINDOW_SIZE[0]-banner_base_size, banner_base_size))
+    
+    new_frame.blit(logo, (0, 0, 50, 50))
+    
+    title = font_title.render("FruitBots", 1, light_grey)
+    new_frame.blit(title, (font_title_size + banner_base_size, banner_base_size//2-(font_title_size//2)))
+    
+    if game_start: time_left = (MAX_TIME - (pygame.time.get_ticks() - start_time)//1000)
+    
+    text = font_title.render("Timer: " + str(time_left) + " s", 1, light_grey)
+    new_frame.blit(text, (WINDOW_SIZE[0] - text.get_width() - font_title_size, banner_base_size//2-(font_title_size//2)))
 
 
-def gameLoop():
-    global done
-    global timer_started
-    global passed_time
-    global num_bananas
-    gamestart = False
-    # bot
-    player = Bot()
-    player.setBoard_size(gridSize - 1)
+def draw_grid():
+    global new_frame
+    global grid
+    for row in range(0, grid_size):
+        for column in range(0, grid_size):
+            #TODO - REVER DESENHO DOS RECTS
+            full_width = margin + width
+            full_height = margin + height
+            pygame.draw.rect(new_frame, white, [full_width * column + grid_offset_x+margin, full_height * row + grid_offset_y+margin,
+                                             width, height], 0)
+            pygame.draw.rect(new_frame, light_grey, [full_width * column + grid_offset_x+margin, full_height * row + grid_offset_y+margin,
+                                             width, height], 1)
+    
+            # draw fruits
+            if grid[row][column] == 1:
+                new_frame.blit(banana_img, ((full_width * column + margin + grid_offset_x + (full_width-img_banana_size)/2), (full_height * row + margin + grid_offset_y+ (full_height-img_banana_size)/2)))
 
-    # fruits specification
-    fruits = list()
-    bananas = list()
-    apples = list()
+     
+def game_loop():
+    global new_frame
+    global time_left
+    global game_start
+    global last_action_time
+    global num_fruits
+    
+    new_frame = pygame.Surface(WINDOW_SIZE)
+    new_frame.fill(white)
+    
+    draw_banner()
+    
+    draw_grid() 
+    
+    if game_start:
+        # new action - random - every second
+        time_elapsed_since_last_action = pygame.time.get_ticks() - last_action_time
+        if time_elapsed_since_last_action >= 1000:
+            grid[bot.row][bot.column] = 0
+            bot.do_random_action()
+            last_action_time = pygame.time.get_ticks()
+        
+        if grid[bot.row][bot.column] == 1:
+            bot.catch_banana()         
+            grid[bot.row][bot.column] = -1
+            num_fruits -= 1
+            
+        if time_left <= 0 or num_fruits == 0:
+            print("here")
+            print(num_fruits)
+            print(time_left)
+            game_start = False
+            new_game()
+        
+    bot.draw(new_frame, margin, width, height, grid_offset_x, grid_offset_y)
+    
+    """   
+    for s in slides:
+        if s.hit:
+            s.move()
+    for s in slides:
+        print("drawing")
+        s.draw()
+    """
+    
+def new_game():
+    
+    global time_left
+    global game_start
+    global start_time
+    global last_action_time
+    global grid
+    global num_fruits
 
-    if not gamestart:
-        for i in range (int(num_bananas.val//1)):
-            r = randint(0,9)
-            c = randint(0,9)
-            if r == 0 and c == 0:
-                r = randint(0, 9)
-                c = randint(0, 9)
-            bananas.append([r, c])
-            fruits.append([r, c])
-
-
-    #apple_pos = [2, 2]
-    #apples.append(apple_pos)
-    #fruits.append(apple_pos)
-
-    # fills the fruits in the board
-    for x in range(0, len(bananas)):
-        grid[bananas[x][0]][bananas[x][1]] = 1
-    for x in range(0, len(apples)):
-        grid[apples[x][0]][apples[x][1]] = 2
-
-    # draws bot image
-    screen.blit(player_img, (((margin + width) * player.column + margin), ((margin + height) * player.row + margin)))
-
-    while True:
-        # grid[player[0]][player[1]] = -1;
-        grid[player.row][player.column] = -1
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                quitGame()
-            if not gamestart:
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    pos = pygame.mouse.get_pos()
-                    for s in slides:
-                        if s.button_rect.collidepoint(pos):
-                            s.hit = True
-                elif event.type == pygame.MOUSEBUTTONUP:
-                    for s in slides:
-                        s.hit = False
-
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_SPACE]:
-                gamestart = True
-                timer_started = not timer_started
-                if timer_started:
-                    start_time = pygame.time.get_ticks()
-            if gamestart:
-                if pressed[pygame.K_q]:
-                    timer_started = False
-                    passed_time = 0
-                    gameLoop()
-
-                if pressed[pygame.K_DOWN]:
-                    grid[player.row][player.column] = 0
-                    player.move_down()
-
-                elif pressed[pygame.K_UP]:
-                    grid[player.row][player.column] = 0
-                    player.move_up()
-
-                elif pressed[pygame.K_RIGHT]:
-                    grid[player.row][player.column] = 0
-                    player.move_right()
-
-                elif pressed[pygame.K_LEFT]:
-                    grid[player.row][player.column] = 0
-                    player.move_left()
-
-                for banana in bananas:
-                    if player.row == banana[0] and player.column == banana[1]:
-                        player.catch_banana()
-                        bananas.remove(banana)
-                        fruits.remove(banana)
-
-        screen.fill((0, 0, 0))
-
-        # draws timer
-        if timer_started:
-            passed_time = pygame.time.get_ticks() - start_time
-        text = font.render("Timer: " + str(passed_time // 1000), True, font_color)
-        screen.blit(text, (WINDOW_SIZE[0] - text.get_width(), 0))
-
-        scoretext = fontScore.render("Score: " + str(player.fruit_count), True, font_color)
-        screen.blit(scoretext, (WINDOW_SIZE[0] / 2 + WINDOW_SIZE[0] / 4, WINDOW_SIZE[1]/2))
-
-        # draw grid and images
-
-        for row in range(0, 10):
-            for column in range(0, 10):
-                color = WHITE
-                use_img = False
-
-                pygame.draw.rect(screen, color, [(margin + width) * column + margin, (margin + height) * row + margin,
-                                                 width, height])
-                if grid[row][column] == -1:
-                    img_toDraw = player_img
-                    use_img = True
-
-                if grid[row][column] == 1:
-                    img_toDraw = banana_img
-                    use_img = True
-                    pygame.draw.rect(screen, RED, [(margin + width) * column + margin, (margin + height) * row + margin,
-                                                   width, height])
-
-                if use_img:
-                    screen.blit(img_toDraw, (((margin + width) * column + margin), ((margin + height) * row + margin)))
-
-        # check if there's no more fruits
-        if len(fruits) == 0:
-            timer_started = False
-            passed_time = 0
-            endGame()
-
-        # check if time has ended
-        if passed_time // 1000 >= 10:
-            timer_started = False
-            passed_time = 0
-            gameOver()
-
-        for s in slides:
-            if s.hit:
-                s.move()
-
-        for s in slides:
-            s.draw()
-
-        clock.tick(60)
-        pygame.display.flip()
+    start_time = pygame.time.get_ticks()
+    time_left = MAX_TIME
+    last_action_time = pygame.time.get_ticks()
+    
+    grid = [[0 for x in range(grid_size)] for y in range(grid_size)]
+    bot.row = bot.column = 0
+    grid[bot.row][bot.column] = -1
+    
+    num_fruits = TOTAL_FRUITS
+    bot.banana_count = bot.fruit_count = 0
+    # place fruits
+    for i in range(num_fruits):
+        pos = np.random.choice(a=grid_size, size=2, replace=False)
+        while pos[0] == 0 and pos[1] == 0:
+            pos = np.random.choice(a=grid_size, size=2, replace=False)
+        
+        grid[pos[0]][pos[1]] = 1
+        
+    print(grid)    
 
 
-def endGame():
-    text = font.render("Game End", True, RED)
-    text_rect = text.get_rect()
-    text_x = WINDOW_SIZE[0] / 2 - text_rect.width / 2
-    text_y = WINDOW_SIZE[1] / 2 - text_rect.height / 2
-    screen.fill((0, 0, 0))
-    screen.blit(text, [text_x, text_y])
+new_game() 
+while not (end):
+    process_pygame_events()
+    
+    game_loop()
+    
+    # actualizar pygame com a nova imagem
+    screen.blit(new_frame, (0, 0))    
+    pygame.display.update()
+    
 
-    while True:
-        for event in pygame.event.get():
-            # print(event)
-            if event.type == pygame.QUIT:
-                quitGame()
-
-        # gameDisplay.fill(white)
-
-        button("PlayAgain", WINDOW_SIZE[0] / 2 - WINDOW_SIZE[0] / 4, WINDOW_SIZE[1] / 2 + WINDOW_SIZE[1] / 4, GREEN,
-               bright_green, gameLoop)
-        button("Quit", WINDOW_SIZE[0] / 2 + WINDOW_SIZE[0] / 4, WINDOW_SIZE[1] / 2 + WINDOW_SIZE[1] / 4, RED,
-               bright_red, quitGame)
-
-        pygame.display.update()
-        clock.tick(15)
-
-
-def gameOver():
-    text = font.render("Game Over", True, RED)
-    text_rect = text.get_rect()
-    text_x = WINDOW_SIZE[0] / 2 - text_rect.width / 2
-    text_y = WINDOW_SIZE[1] / 2 - text_rect.height / 2
-    screen.fill((0, 0, 0))
-    screen.blit(text, [text_x, text_y])
-
-    while True:
-        for event in pygame.event.get():
-            # print(event)
-            if event.type == pygame.QUIT:
-                quitGame()
-
-        # gameDisplay.fill(white)
-
-        button("Retry", WINDOW_SIZE[0] / 2 - WINDOW_SIZE[0] / 4, WINDOW_SIZE[1] / 2 + WINDOW_SIZE[1] / 4, GREEN,
-               bright_green, gameLoop)
-        button("Quit", WINDOW_SIZE[0] / 2 + WINDOW_SIZE[0] / 4, WINDOW_SIZE[1] / 2 + WINDOW_SIZE[1] / 4, RED,
-               bright_red, quitGame)
-
-        pygame.display.update()
-        clock.tick(15)
-
-
-def quitGame():
-    pygame.quit()
-    quit()
-
-
-# UI FUNCTIONS-------------------------
-
-# text used on objects
-def text_objects(text, font):
-    textSurface = font.render(text, True, (0, 0, 0))
-    return textSurface, textSurface.get_rect()
-
-
-def button(message, x, y, ic, ac, action=None):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    smallText = pygame.font.SysFont("comicsansms", 32)
-    textSurf, textRect = text_objects(message, smallText)
-    w = textRect.width
-    h = textRect.height
-    if x + w > mouse[0] > x and y + h > mouse[1] > y:
-        pygame.draw.rect(screen, ac, (x, y, w, h))
-        if click[0] == 1 and action is not None:
-            action()
-    else:
-        pygame.draw.rect(screen, ic, (x, y, w, h))
-
-    textRect.center = ((x + (w / 2)), (y + (h / 2)))
-    screen.blit(textSurf, textRect)
-
-
-gameLoop()
+pygame.quit()
+quit()
